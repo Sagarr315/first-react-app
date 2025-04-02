27acf9b4b4a1ffe4660602786d1dc0c2ce2efb8e
Sagarr315 <sagarrbhor715@gmail.com>
2025-04-02 12:48:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-
 import './App.css';
-
+import Item from './components/Item.js'; // Corrected import statement
 
 function App() {
   return ( 
    <div>
-  
-   <div className="App"> Hello Worl..!</div>
+    <Item></Item> 
+     <div className="App">Hello World..!</div>
    </div>
-);
+  );
 }
+
 export default App;
