5dd9f156e0349143839cf3b008f011ad5081f00e
Sagarr315 <sagarrbhor715@gmail.com>
2025-04-04 20:43:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
-import Item from './components/Item.js'; // Corrected import statement
+import Item from './components/Item.js'; 
+import ItemDate from './components/ItemDate.js';
 
 function App() {
   return ( 
    <div>
     <Item></Item> 
+    <ItemDate></ItemDate>
+
+    <Item></Item> 
+    <ItemDate></ItemDate>
+
+    <Item></Item> 
+    <ItemDate></ItemDate>
      <div className="App">Hello World..!</div>
    </div>
   );
