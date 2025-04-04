4325990d6e05d8643166c87fe30143812ea6062b
Sagarr315 <sagarrbhor715@gmail.com>
2025-04-04 20:43:46 +0530

    Create ItemDate.js

---
+++ src/components/ItemDate.js
@@ -0,0 +1,18 @@
+import './ItemDate.css';
+
+function ItemDate (){
+
+const day = 20;
+const month = 'may';
+const yeaar = 156;
+
+    return(
+        <div className='colour-design-back'>
+            <p>{month}</p>
+            <p>jan</p>
+            <p>1978</p>
+        </div>
+    )
+}
+
+export default ItemDate ;
