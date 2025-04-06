c1ea96764a2541cf5a830c55ced5350ca506b6be
Sagarr315 <sagarrbhor715@gmail.com>
2025-04-06 17:33:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import Item from './components/Item.js';
 import ItemDate from './components/ItemDate.js';
 
 function App() {
+  const itemnamechanges="SIrxcel";
   return ( 
    <div>
-    <Item></Item> 
-    <ItemDate></ItemDate>
+    <Item name ='nirma'></Item> 
+    <ItemDate day='5' month='jan' year='1976' ></ItemDate>
 
-    <Item></Item> 
-    <ItemDate></ItemDate>
+    <Item name={itemnamechanges}></Item> 
+    <ItemDate day='6' month='jan' year='1977' ></ItemDate>
 
-    <Item></Item> 
-    <ItemDate></ItemDate>
+    <Item name='tide'></Item> 
+    <ItemDate day='7' month='jan' year='1978'></ItemDate>
      <div className="App">Hello World..!</div>
    </div>
   );
