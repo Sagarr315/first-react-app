0af46c380c95aadf56c4267a0adf8fd39b6b8867
Sagarr315 <sagarrbhor715@gmail.com>
2025-04-09 12:12:53 +0530

    Update Products.js

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,39 +1,35 @@
 import React from 'react';
 
-import ProductItem from './ProductItem';
+import ProductItem from './ProductsItem.js';
 import Card from './Card.js';
 
 import './Products.css';
 
-const Products= (props) => {
-
-    return(
-            <Card className='style'>
-                <ProductItem>
-                    title={props.item[0].title}
-                    amount={props.amount[0].amount}
-                    Date={props.Date[0].Date}
-               </ProductItem>
-              
-               <ProductItem>
-                    title={props.item[1].title}
-                    amount={props.amount[1].amount}
-                    Date={props.Date[1].Date}
-               </ProductItem>
-                
-                <ProductItem>
-                    title={props.item[2].title}
-                    amount={props.amount[2].amount}
-                    Date={props.Date[2].Date}
-               </ProductItem>
-
-               <ProductItem>
-                    title={props.item[3].title}
-                    amount={props.amount[3].amount}
-                    Date={props.Date[3].Date}
-               </ProductItem>
-            </Card>
-    );
-}
+const Products = (props) => {
+  return (
+    <Card className='style'>
+      <ProductItem
+        title={props.items[0].title}
+        amount={props.items[0].amount}
+        date={props.items[0].date}
+      />
+      <ProductItem
+        title={props.items[1].title}
+        amount={props.items[1].amount}
+        date={props.items[1].date}
+      />
+      <ProductItem
+        title={props.items[2].title}
+        amount={props.items[2].amount}
+        date={props.items[2].date}
+      />
+      <ProductItem
+        title={props.items[3].title}
+        amount={props.items[3].amount}
+        date={props.items[3].date}
+      />
+    </Card>
+  );
+};
 
 export default Products;
