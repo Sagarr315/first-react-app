4b38c16e3bf5dd62a60ed64204e025566d000f0c
Sagarr315 <sagarrbhor715@gmail.com>
2025-04-09 12:19:56 +0530

    Update ProductDate.js

--- src/components/ProductDate.js
+++ src/components/ProductDate.js
@@ -3,12 +3,12 @@ import React from 'react';
 import './ProductsDate.css';
 
 const ProductDate = (props) => {
-    const month = props.date.toLocalString('en-US',{ month:"long" })
-    const day = props.date.toLocalString('en-US',{ day:"2-digit" })
+    const month = props.date.toLocaleString('en-US',{ month:"long" })
+    const day = props.date.toLocaleString('en-US',{ day:"2-digit" })
     const year = props.date.getFullYear();
 
     return (
-        <div classNmae="product_data">
+        <div className="product_data">
             <div className="product_month">{month}</div>
             <div className="product_month">{year}</div>
             <div className="product_month">{day}</div>
