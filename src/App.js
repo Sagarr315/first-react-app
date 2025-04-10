d2f5226aa3ba1f7e1bf10ad98a67acb8104a2fb1
Sagarr315 <sagarrbhor715@gmail.com>
2025-04-10 17:14:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,19 +13,19 @@ const App = () => {
       id: "p2",
       title: "sirexcel",
       amount: 140,
-      date: new Date(2022, 10, 2),
+      date: new Date(2013, 1, 15),
     },
     {
       id: "p3",
       title: "tide",
       amount: 120,
-      date: new Date(2022, 10, 2),
+      date: new Date(1902, 0, 20),
     },
     {
-      id: "p4", // ✅ changed to unique id
+      id: "p4", 
       title: "maggii",
       amount: 130,
-      date: new Date(2022, 10, 2),
+      date: new Date(1976, 11, 11),
     }
   ];
 
